b64bab8bdb5446748698ca4b0467f60d2666bdb5
TheAustrian1998 <theaustrianeth@gmail.com>
2021-09-22 16:12:22 -0300

    Setting up env,

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+
+let { privateKey, alchemyUrl, ftmscanApiKey } = require("./secrets.json");
+
 module.exports = {
   solidity: "0.8.7",
+  networks: {
+    fantom: {
+      url: alchemyUrl,
+      accounts: [privateKey]
+    }
+  },
+  etherscan: {
+    apiKey: ftmscanApiKey
+  },
+  gasReporter: {
+    excludeContracts: [""]
+  }
 };
